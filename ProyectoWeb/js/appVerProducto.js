f9ebbb9016e1cb90857fa3d66b7b1ac16b7797db
johansel64 <62160555+johansel64@users.noreply.github.com>
2021-08-18 21:53:02 -0600

    Add files via upload
    js de productos

--- ProyectoWeb/js/appVerProducto.js
+++ ProyectoWeb/js/appVerProducto.js
@@ -25,8 +25,14 @@ function productoHTML(){
          const row = document.createElement('article');
          row.innerHTML = `
              <div class="row text-center pt-5">
-                 <img class=" ver-producto col-6" src="${imagen}" alt="Card image cap">
-                <div class='col-6 border'>
+                 <img class=" ver-producto col-lg-6" src="${imagen}" alt="Card image cap">
+                <div class='col-lg-6 border'>
+                <div class="row">
+
+                    <img class="col-6" src="${imagen}" >
+
+                    <img class="col-6" src="${imagen}" >
+                </div>
                 <table class="table">
                     <tr>
                         <th><h1>${titulo}</h1></th>
@@ -38,9 +44,11 @@ function productoHTML(){
                         <th><h4><span>Precio: ¢</span> <var>${precio}</var> </h4></th>
                     </tr>
                     <tr>
-                        <th><p><span> En salón <strong>Karolina</strong> podes encontrar miles de prodcutos como este increible <strong> ${titulo}</strong> a por tan solo un precio de <var>$${precio} </var> colones.</span></p></th>
+                        <th><p><span> En salón <strong>Karolina</strong> podes encontrar miles de prodcutos como este increible <strong> ${titulo}</strong> a por tan solo un precio de $<var>${precio} </var> colones.</span></p></th>
                     </tr>
                 </table>
+
+                <a href="#" class="btn bg-primary col-12 agregar-carrito" data-id="${id}"><i class="fas fa-shopping-cart"></i></a>
                     
                     
                     
